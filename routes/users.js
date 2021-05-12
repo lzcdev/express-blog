63f25e5050a3f63277cad19e17af6a82c33a205e
lzcdev <lzc2134@gmail.com>
2021-05-12 10:34:48 +0800

    feat: initial commit

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/cool', function(req, res,next) {
-  res.send('你好酷')
-})
+
 module.exports = router;
